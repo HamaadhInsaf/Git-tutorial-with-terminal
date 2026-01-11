a5d27b25025212c7efaa58255058d1efdf88d904
Hamaadh <hamaadh.insaf14@gmail.com>
2026-01-11 17:12:56 +0530

    Added a TODO

--- Program.py
+++ Program.py
@@ -1,4 +1,5 @@
 from random import randint
 randomNumber = randint(1, 100)
 userNumber = int(input("Enter a number - "))
-print(f"Random number: {randomNumber}\n{randomNumber} + {userNumber} = {randomNumber + userNumber}")
+print(f"Random number: {randomNumber}\n{randomNumber} + {userNumber} = {randomNumber + userNumber}\n{randomNumber} - {userNumber} = ?\n{randomNumber} * {userNumber} = ?\n{randomNumber} / {userNumber} = ?")
+#TODO: Complete the print statement
