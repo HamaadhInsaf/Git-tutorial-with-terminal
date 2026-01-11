de3ed399c4047f55581926c9cb91ba01d9900e6e
Hamaadh <hamaadh.insaf14@gmail.com>
2026-01-11 17:21:14 +0530

    Added a question

--- Program.py
+++ Program.py
@@ -3,3 +3,4 @@ randomNumber = randint(1, 100)
 userNumber = int(input("Enter a number - "))
 print(f"Random number: {randomNumber}\n{randomNumber} + {userNumber} = {randomNumber + userNumber}\n{randomNumber} - {userNumber} = ?\n{randomNumber} * {userNumber} = ?\n{randomNumber} / {userNumber} = ?")
 #TODO: Complete the print statement
+print("My name is [name]")
